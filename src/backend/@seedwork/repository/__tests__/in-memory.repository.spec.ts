6ae950816456425700a87c7f28c4cc83b40dd699
Rafael Borges <rafael.borges@luizalabs.com>
2022-12-11 15:36:47 -0300

    test in memory repo

--- src/backend/@seedwork/repository/__tests__/in-memory.repository.spec.ts
+++ src/backend/@seedwork/repository/__tests__/in-memory.repository.spec.ts
@@ -48,13 +48,55 @@ describe('In Memory Repository', () => {
     });
   });
 
-  // describe('update', () => {
-  //   it('should update', async () => {
-  //     repository.items = [entity_1, entity_2];
+  describe('update', () => {
+    it('should update', async () => {
+      repository.items = [entity_1, entity_2];
 
-  //     const updated_entity_2 = entity_2
-  //     const result = await repository.update()
-  //     expect(result).toStrictEqual([entity_1, entity_2]);
-  //   });
-  // });
+      const updated_entity_2 = new StubEntity(
+        { name: 'updated name', price: 99.9 },
+        entity_2.id,
+      );
+      await repository.update(updated_entity_2);
+
+      expect(repository.items).toStrictEqual([entity_1, updated_entity_2]);
+    });
+
+    it('should throw error when dont find entity', async () => {
+      repository.items = [entity_1, entity_2];
+
+      const updated_entity_2 = new StubEntity(
+        { name: 'updated name', price: 99.9 },
+        '271f07b0-4964-4081-a4dc-a27f31b3eaff',
+      );
+
+      expect(
+        async () => await repository.update(updated_entity_2),
+      ).rejects.toThrow(
+        new NotFoundError(
+          `Entity ID: 271f07b0-4964-4081-a4dc-a27f31b3eaff not found`,
+        ),
+      );
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete', async () => {
+      repository.items = [entity_1, entity_2];
+      await repository.delete(entity_1.id);
+      expect(repository.items).toStrictEqual([entity_2]);
+    });
+
+    it('should throw error when dont find entity', async () => {
+      repository.items = [entity_1, entity_2];
+
+      expect(
+        async () =>
+          await repository.delete('271f07b0-4964-4081-a4dc-a27f31b3eaff'),
+      ).rejects.toThrow(
+        new NotFoundError(
+          `Entity ID: 271f07b0-4964-4081-a4dc-a27f31b3eaff not found`,
+        ),
+      );
+    });
+  });
 });
