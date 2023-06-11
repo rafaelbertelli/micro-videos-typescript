59670c93de36cb034521baf068376072a9dcb50b
Rafael Borges <rafael.borges@luizalabs.com>
2023-06-11 16:24:29 -0300

    register providers

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,9 +1,49 @@
 import { Module } from '@nestjs/common';
+import {
+  CreateCategoryUsecase,
+  FindAllCategoriesUsecase,
+  FindByIdCategoryUsecase,
+  UpdateCategoryUsecase,
+} from '../../backend/category/application';
+import { CategoryRepository } from '../../backend/category/domain';
+import { CategoryInMemoryRepository } from '../../backend/category/infra';
 import { CategoryController } from './category.controller';
-import { CategoryService } from './category.service';
 
 @Module({
   controllers: [CategoryController],
-  providers: [CategoryService],
+  providers: [
+    {
+      provide: 'CategoryRepository',
+      useClass: CategoryInMemoryRepository,
+    },
+    {
+      provide: CreateCategoryUsecase,
+      useFactory: (categoryRepository: CategoryRepository.Repository) => {
+        return new CreateCategoryUsecase(categoryRepository);
+      },
+      inject: ['CategoryRepository'],
+    },
+    {
+      provide: FindAllCategoriesUsecase,
+      useFactory: (categoryRepository: CategoryRepository.Repository) => {
+        return new FindAllCategoriesUsecase(categoryRepository);
+      },
+      inject: ['CategoryRepository'],
+    },
+    {
+      provide: FindByIdCategoryUsecase,
+      useFactory: (categoryRepository: CategoryRepository.Repository) => {
+        return new FindByIdCategoryUsecase(categoryRepository);
+      },
+      inject: ['CategoryRepository'],
+    },
+    {
+      provide: UpdateCategoryUsecase,
+      useFactory: (categoryRepository: CategoryRepository.Repository) => {
+        return new UpdateCategoryUsecase(categoryRepository);
+      },
+      inject: ['CategoryRepository'],
+    },
+  ],
 })
 export class CategoryModule {}
