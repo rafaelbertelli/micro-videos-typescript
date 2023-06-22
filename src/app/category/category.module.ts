1b42b0ad75e267fba45e41fbe63c7a1f9aa680c4
Rafael Borges <rafael.borges@luizalabs.com>
2023-06-22 20:52:54 -0300

    split providers into another file

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,65 +1,60 @@
 import { Module } from '@nestjs/common';
-import {
-  CreateCategoryUsecase,
-  DeleteCategoryUsecase,
-  FindAllCategoriesUsecase,
-  FindByIdCategoryUsecase,
-  SearchCategoriesUsecase,
-  UpdateCategoryUsecase,
-} from '../../backend/category/application';
-import { CategoryRepository } from '../../backend/category/domain';
-import { CategoryInMemoryRepository } from '../../backend/category/infra';
 import { CategoryController } from './category.controller';
+import { CATEGORY_PROVIDERS } from './category.providers';
 
 @Module({
   controllers: [CategoryController],
   providers: [
-    {
-      provide: 'CategoryRepository',
-      useClass: CategoryInMemoryRepository,
-    },
-    {
-      provide: CreateCategoryUsecase,
-      useFactory: (categoryRepository: CategoryRepository.Repository) => {
-        return new CreateCategoryUsecase(categoryRepository);
-      },
-      inject: ['CategoryRepository'],
-    },
-    {
-      provide: FindAllCategoriesUsecase,
-      useFactory: (categoryRepository: CategoryRepository.Repository) => {
-        return new FindAllCategoriesUsecase(categoryRepository);
-      },
-      inject: ['CategoryRepository'],
-    },
-    {
-      provide: SearchCategoriesUsecase,
-      useFactory: (categoryRepository: CategoryRepository.Repository) => {
-        return new SearchCategoriesUsecase(categoryRepository);
-      },
-      inject: ['CategoryRepository'],
-    },
-    {
-      provide: FindByIdCategoryUsecase,
-      useFactory: (categoryRepository: CategoryRepository.Repository) => {
-        return new FindByIdCategoryUsecase(categoryRepository);
-      },
-      inject: ['CategoryRepository'],
-    },
-    {
-      provide: UpdateCategoryUsecase,
-      useFactory: (categoryRepository: CategoryRepository.Repository) => {
-        return new UpdateCategoryUsecase(categoryRepository);
-      },
-      inject: ['CategoryRepository'],
-    },
-    {
-      provide: DeleteCategoryUsecase,
-      useFactory: (categoryRepository: CategoryRepository.Repository) => {
-        return new DeleteCategoryUsecase(categoryRepository);
-      },
-      inject: ['CategoryRepository'],
-    },
+    ...Object.values(CATEGORY_PROVIDERS.REPOSITORIES),
+    ...Object.values(CATEGORY_PROVIDERS.USE_CASES),
   ],
+  // providers: [
+  //   {
+  //     provide: 'CategoryRepository',
+  //     useClass: CategoryInMemoryRepository,
+  //   },
+  //   {
+  //     provide: CreateCategoryUsecase,
+  //     useFactory: (categoryRepository: CategoryRepository.Repository) => {
+  //       return new CreateCategoryUsecase(categoryRepository);
+  //     },
+  //     inject: ['CategoryRepository'],
+  //   },
+  //   {
+  //     provide: FindAllCategoriesUsecase,
+  //     useFactory: (categoryRepository: CategoryRepository.Repository) => {
+  //       return new FindAllCategoriesUsecase(categoryRepository);
+  //     },
+  //     inject: ['CategoryRepository'],
+  //   },
+  //   {
+  //     provide: SearchCategoriesUsecase,
+  //     useFactory: (categoryRepository: CategoryRepository.Repository) => {
+  //       return new SearchCategoriesUsecase(categoryRepository);
+  //     },
+  //     inject: ['CategoryRepository'],
+  //   },
+  //   {
+  //     provide: FindByIdCategoryUsecase,
+  //     useFactory: (categoryRepository: CategoryRepository.Repository) => {
+  //       return new FindByIdCategoryUsecase(categoryRepository);
+  //     },
+  //     inject: ['CategoryRepository'],
+  //   },
+  //   {
+  //     provide: UpdateCategoryUsecase,
+  //     useFactory: (categoryRepository: CategoryRepository.Repository) => {
+  //       return new UpdateCategoryUsecase(categoryRepository);
+  //     },
+  //     inject: ['CategoryRepository'],
+  //   },
+  //   {
+  //     provide: DeleteCategoryUsecase,
+  //     useFactory: (categoryRepository: CategoryRepository.Repository) => {
+  //       return new DeleteCategoryUsecase(categoryRepository);
+  //     },
+  //     inject: ['CategoryRepository'],
+  //   },
+  // ],
 })
 export class CategoryModule {}
---
+++ src/app/category/category.providers.ts
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import {
+  CreateCategoryUsecase,
+  DeleteCategoryUsecase,
+  FindAllCategoriesUsecase,
+  FindByIdCategoryUsecase,
+  SearchCategoriesUsecase,
+  UpdateCategoryUsecase,
+} from '../../backend/category/application';
+import { CategoryRepository } from '../../backend/category/domain';
+import { CategoryInMemoryRepository } from '../../backend/category/infra';
+
+export namespace CATEGORY_PROVIDERS {
+  export namespace REPOSITORIES {
+    export const CATEGORY_IN_MEMORY_REPOSITORY = {
+      provide: 'CategoryRepository',
+      useClass: CategoryInMemoryRepository,
+    };
+  }
+
+  export namespace USE_CASES {
+    export const CREATE_CATEGORY_USECASE = {
+      provide: CreateCategoryUsecase,
+      useFactory: (categoryRepository: CategoryRepository.Repository) => {
+        return new CreateCategoryUsecase(categoryRepository);
+      },
+      inject: [REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide],
+    };
+
+    export const FIND_ALL_CATEGORIES_USECASE = {
+      provide: FindAllCategoriesUsecase,
+      useFactory: (categoryRepository: CategoryRepository.Repository) => {
+        return new FindAllCategoriesUsecase(categoryRepository);
+      },
+      inject: [REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide],
+    };
+
+    export const SEARCH_CATEGORIES_USECASE = {
+      provide: SearchCategoriesUsecase,
+      useFactory: (categoryRepository: CategoryRepository.Repository) => {
+        return new SearchCategoriesUsecase(categoryRepository);
+      },
+      inject: [REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide],
+    };
+
+    export const FIND_BY_ID_CATEGORY_USECASE = {
+      provide: FindByIdCategoryUsecase,
+      useFactory: (categoryRepository: CategoryRepository.Repository) => {
+        return new FindByIdCategoryUsecase(categoryRepository);
+      },
+      inject: [REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide],
+    };
+
+    export const UPDATE_CATEGORY_USECASE = {
+      provide: UpdateCategoryUsecase,
+      useFactory: (categoryRepository: CategoryRepository.Repository) => {
+        return new UpdateCategoryUsecase(categoryRepository);
+      },
+      inject: [REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide],
+    };
+
+    export const DELETE_CATEGORY_USECASE = {
+      provide: DeleteCategoryUsecase,
+      useFactory: (categoryRepository: CategoryRepository.Repository) => {
+        return new DeleteCategoryUsecase(categoryRepository);
+      },
+      inject: [REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide],
+    };
+  }
+}
