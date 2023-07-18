7e65e7c650f68a60b13eb3ca7e98af242407eb33
Rafael Borges <rafael.borges@luizalabs.com>
2023-07-18 19:43:24 -0300

    implement and test bulkCreate

--- src/backend/@seedwork/infra/sequelize/__tests__/sequelize-model.factory.spec.ts
+++ src/backend/@seedwork/infra/sequelize/__tests__/sequelize-model.factory.spec.ts
@@ -4,7 +4,6 @@ import {
   DataType,
   Model,
   PrimaryKey,
-  Sequelize,
   Table,
 } from 'sequelize-typescript';
 import { validate as uuidValidate } from 'uuid';
@@ -35,6 +34,10 @@ class StubModel extends Model {
 describe('SequelizeModelFactory', () => {
   setupSequelize({ models: [StubModel] });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(SequelizeModelFactory).toBeDefined();
   });
@@ -44,7 +47,7 @@ describe('SequelizeModelFactory', () => {
     expect(uuidValidate(model.id)).toBeTruthy();
     expect(model.id).not.toBeNull();
     expect(model.name).not.toBeNull();
-    expect(StubModel.mockFactory).toHaveBeenCalled();
+    expect(StubModel.mockFactory).toHaveBeenCalledTimes(1);
 
     const modelFound = await StubModel.findByPk(model.id);
     expect(modelFound).toBeDefined();
@@ -64,7 +67,7 @@ describe('SequelizeModelFactory', () => {
     expect(uuidValidate(model.id)).toBeTruthy();
     expect(model.id).not.toBeNull();
     expect(model.name).not.toBeNull();
-    expect(StubModel.mockFactory).toHaveBeenCalled();
+    expect(StubModel.mockFactory).toHaveBeenCalledTimes(1);
 
     const params = {
       id: chance.guid({ version: 4 }),
@@ -73,5 +76,74 @@ describe('SequelizeModelFactory', () => {
     const modelWithData = await StubModel.factory().make(params);
     expect(modelWithData.toJSON().id).toEqual(params.id);
     expect(modelWithData.toJSON().name).toEqual(params.name);
+  });
+
+  test('bulkCreate method using empty param', async () => {
+    const models = await StubModel.factory().bulkCreate();
+
+    expect(models).toHaveLength(1);
+    expect(uuidValidate(models[0].id)).toBeTruthy();
+    expect(models[0].id).not.toBeNull();
+    expect(models[0].name).not.toBeNull();
+    expect(StubModel.mockFactory).toHaveBeenCalledTimes(1);
+
+    const modelFound = await StubModel.findByPk(models[0].id);
+    expect(modelFound.toJSON().id).toEqual(models[0].id);
+    expect(modelFound.toJSON().name).toEqual(models[0].name);
+  });
+
+  test('bulkCreate method using one param', async () => {
+    const params = {
+      id: chance.guid({ version: 4 }),
+      name: chance.name(),
+    };
+
+    const models = await StubModel.factory().bulkCreate(() => params);
+
+    expect(models).toHaveLength(1);
+    expect(uuidValidate(models[0].id)).toBeTruthy();
+    expect(models[0].id).not.toBeNull();
+    expect(models[0].name).not.toBeNull();
+    expect(StubModel.mockFactory).not.toHaveBeenCalled();
+
+    const modelFound = await StubModel.findByPk(params.id);
+    expect(modelFound.toJSON().id).toEqual(params.id);
+    expect(modelFound.toJSON().name).toEqual(params.name);
+  });
+
+  test('bulkCreate method using empty param and count gt 1', async () => {
+    const models = await StubModel.factory().count(5).bulkCreate();
+
+    expect(models).toHaveLength(5);
+    expect(uuidValidate(models[0].id)).toBeTruthy();
+    expect(models[0].id).not.toBeNull();
+    expect(models[0].name).not.toBeNull();
+    expect(StubModel.mockFactory).toHaveBeenCalledTimes(5);
+
+    const modelFound1 = await StubModel.findByPk(models[0].id);
+    expect(modelFound1.toJSON().id).toEqual(models[0].id);
+    expect(modelFound1.toJSON().name).toEqual(models[0].name);
+
+    const modelFound3 = await StubModel.findByPk(models[2].id);
+    expect(modelFound3.toJSON().id).toEqual(models[2].id);
+    expect(modelFound3.toJSON().name).toEqual(models[2].name);
+  });
+
+  test('bulkCreate method using one param and count gt 1', async () => {
+    const models = await StubModel.factory()
+      .count(2)
+      .bulkCreate(() => ({
+        id: chance.guid({ version: 4 }),
+        name: chance.name(),
+      }));
+
+    expect(models).toHaveLength(2);
+    expect(uuidValidate(models[0].id)).toBeTruthy();
+    expect(models[0].id).not.toBeNull();
+    expect(models[0].name).not.toBeNull();
+    expect(StubModel.mockFactory).not.toHaveBeenCalledTimes(2);
+
+    expect(models[0].id).not.toEqual(models[1].id);
+    expect(models[0].name).not.toEqual(models[1].name);
   });
 });
--- src/backend/@seedwork/infra/sequelize/sequelize-model.factory.ts
+++ src/backend/@seedwork/infra/sequelize/sequelize-model.factory.ts
@@ -1,22 +1,28 @@
 export class SequelizeModelFactory {
   private _count = 1;
 
-  constructor(private model, private factoryProps: () => any) {}
-
-  async create(data?) {
-    return this.model.create(data ?? this.factoryProps());
-  }
-
-  make(data?) {
-    return this.model.build(data ?? this.factoryProps());
-  }
-
   count(count: number) {
     this._count = count;
     return this;
   }
 
-  async bulkCreate() {}
+  constructor(private model, private defaultFactoryProps: () => any) {}
+
+  async create(data?) {
+    return this.model.create(data ?? this.defaultFactoryProps());
+  }
+
+  make(data?) {
+    return this.model.build(data ?? this.defaultFactoryProps());
+  }
+
+  async bulkCreate(factoryProps?: (index: number) => any) {
+    const data = new Array(this._count)
+      .fill(factoryProps ? factoryProps : this.defaultFactoryProps)
+      .map((factory, index) => factory(index));
+
+    return this.model.bulkCreate(data);
+  }
 
   bulkMake() {}
 }
